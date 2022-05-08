61e0cb25ef94e87db51cadda5507a440c78c60f4
Ailton Cordeiro <ailtoncordeiro@gmail.com>
2022-05-08 00:19:00 -0300

    🚧 Added Delete User

--- app/views/users.py
+++ app/views/users.py
@@ -5,7 +5,7 @@ from flask import request, jsonify
 from ..models.users import Users, user_schema, users_schema
 
 def post_user():
-    
+    """ Create user """
     username = request.json['username']
     password = request.json['password']
     name = request.json['name']
@@ -20,7 +20,7 @@ def post_user():
         return jsonify({"message":"unable to create","data":{}}), 500
 
 def update_user(id):
-
+    """ Update User """
     username = request.json['username']
     password = request.json['password']
     name = request.json['name']
@@ -38,8 +38,8 @@ def update_user(id):
     except:
         return jsonify({"message":"unable to update","data":{}}), 500
 
-def get_user():
-
+def get_users():
+    """ Get all users """
     users = Users.query.all()
     if users:
         result = users_schema.dump(users)
@@ -47,3 +47,26 @@ def get_user():
     
     return jsonify({"message":"nothing found", "data":{}})
 
+def get_user(id):
+    """ Get user """
+    users = Users.query.get(id)
+    if users:
+        result = user_schema.dump(users)
+        return jsonify({"message":"successfully fetched", "data": result})
+    
+    return jsonify({"message":"user don't exist", "data":{}})
+
+def delete_user(id):
+    """ Delete User """
+    user = Users.query.get(id)
+    if not user:
+        return jsonify({'message': "user don't exist", 'data': {}}), 404
+
+    if user:
+        try:
+            db.session.delete(user)
+            db.session.commit()
+            result = user_schema.dump(user)
+            return jsonify({'message': 'successfully deleted', 'data': result}), 200
+        except:
+            return jsonify({'message': 'unable to delete', 'data': {}}), 500
